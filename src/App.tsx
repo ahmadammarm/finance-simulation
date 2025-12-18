446fd2129291e24b8e5e205d597ebe8931e8451d
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-12-18 20:57:58 +0700

    feat: add shadcn/ui component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
     return (
-        <div className="text-3xl font-bold underline">
-            Financa Simulation
+        <div>
+            <div className="flex min-h-screen flex-col items-center justify-center p-4">
+                <Button>Finance Simulation</Button>
+            </div>
         </div>
     )
 }
