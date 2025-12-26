741c1013039da8937333ae9cd3fa29ed0ad51c1f
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-12-26 09:11:59 +0700

    refactor: set light mode as default mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "./providers/theme-provider"
 
 function App({ children }: { children: React.ReactNode }) {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
             {children}
         </ThemeProvider>
     )
